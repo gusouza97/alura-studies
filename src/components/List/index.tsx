50ee26759fee57c0b3665699f932637bc39d0261
Gustavo <gustavosilva419@gmail.com>
2022-06-24 17:18:18 -0300

    Renderizando um Array

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,24 +1,32 @@
 function List() {
+
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    }, 
+    {
+        tarefa: 'Javascript',
+        tempo: '01:00:00'
+    }, 
+    {
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
+    }]
+
   return (
     <aside>
         <h2>Estudos do dia</h2>
         <ul>
-            <li>
-                <h3>
-                    React
-                </h3>
-                <span>
-                    02:00:00
-                </span>
-            </li>
-            <li>
-                <h3>
-                     Javascript   
-                </h3>
-                <span>
-                    01:00:00
-                </span>
-            </li>
+            {tarefas.map((value, index) => (
+                <li key={index}>
+                    <h3>
+                        {value.tarefa}
+                    </h3>
+                    <span>
+                        {value.tempo}
+                    </span>
+                </li>
+            ))}
         </ul>
     </aside>
   )
