51b4ee5136bb478e1fdee69f83da5ab40ccda53b
Gustavo <gustavosilva419@gmail.com>
2022-06-24 22:44:49 -0300

    criacao do utilitario tempo para segundos

--- src/components/Cronometer/index.tsx
+++ src/components/Cronometer/index.tsx
@@ -1,8 +1,10 @@
+import { tempoParaSegundos } from "../../common/utils/date";
 import Button from "../Button"
 import Clock from "./Clock"
 import style from "./Cronometer.module.scss";
 
 function Cronometer() {
+  console.log('conversao: ', tempoParaSegundos('01:01:01'))
   return (
     <div className={style.cronometro}>
         <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
